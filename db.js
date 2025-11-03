d2d4337f4839e82813d65dba165251d7708b8938
Paris Vigay <parisvigay@pariss-mbp.home>
2025-11-03 15:19:12 +0000

    Commit prior to me switching onto Meg's machine

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
+const connectionString = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(connectionString, supabaseKey)
 
 export default supabase;
